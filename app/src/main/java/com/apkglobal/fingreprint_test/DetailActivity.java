89f0039890f5777b30080e1dc149a23ffe31b696
Saurabh <saurabh1065@gmail.com>
2017-06-27 12:31:19 +0530

    working fine

---
+++ app/src/main/java/com/apkglobal/fingreprint_test/DetailActivity.java
@@ -0,0 +1,13 @@
+package com.apkglobal.fingreprint_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
